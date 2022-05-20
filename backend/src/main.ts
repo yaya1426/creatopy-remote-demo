35d920839c863b11d03256907bfb1a1e708bded5
Yahya Nabil ElAraby <yaya1426@gmail.com>
2022-05-20 13:34:23 +0200

    Backend: Update port number to come dynamically from configService

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,17 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from './config/config.service';
+
+const logger = new Logger('Backend');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors();
+  // Load the port from the config service.
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get('BACKEND_PORT');
+  await app.listen(port);
+  logger.log(`## Backend is listening on ${port}`);
 }
 bootstrap();
