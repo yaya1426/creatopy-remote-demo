7c3335a005ea45112c8dd7a02a0a570b82607dd9
Yahya Nabil ElAraby <yaya1426@gmail.com>
2022-05-20 17:45:42 +0200

    Backend: Update prettier/lint settings

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -6,7 +6,10 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
   root: true,
   env: {
     node: true,
@@ -19,5 +22,6 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
   },
 };
